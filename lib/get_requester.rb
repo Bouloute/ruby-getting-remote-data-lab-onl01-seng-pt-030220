46d25c55579a1d6f802840472d8b7aab5e35a0cb
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-10-23 15:56:56 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,4 +1,6 @@
-class GetRequester
+require 'open-uri'
+uri = URI.parse(url)class GetRequester
+
   attr_reader :url
   
   def initialize(url)
@@ -6,6 +8,6 @@ class GetRequester
   end
   
   def get_response_body()
-    
+    uri = URI.parse(url)class GetRequester
   end
 end
